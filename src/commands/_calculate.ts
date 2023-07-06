f6156b7108c2a968f22eb220dfa0a3c18b44d718
glemiere <guillaumelm.in@gmail.com>
2023-07-05 20:09:30 -0600

    calculate function ready

--- src/commands/_calculate.ts
+++ src/commands/_calculate.ts
@@ -9,9 +9,46 @@ export default class Calculate implements Command {
         while (true) {
             try {
                 input = await new Prompt('Enter operation (or "exit" to quit):').exec();
+                logger.success(this._calculate(input));
+
             } catch (error) {
                 logger.error(error);
             }
         }
+    }
+
+    private _calculate(input: string) :number {
+        const splitByOperators: RegExp = /(\*|\/|\+|\-)/;
+        const assignType: any = (element: string) => isNaN(parseFloat(element)) ? element : parseFloat(element);
+        const elements: Array<any> = input.split(splitByOperators).map(assignType);
+        const operationsInOrder: Array<Record<string, (a: number, b: number) => number>> = [
+            {
+                '*': (a: number, b: number) => a * b,
+                '/': (a: number, b: number) => a / b
+            },
+            {
+                '+': (a: number, b: number) => a + b,
+                '-': (a: number, b: number) => a - b
+            }
+        ];
+
+        operationsInOrder.forEach(operators => {
+            // Starting from the first operator.
+            let i = 1;
+            
+            while (i < elements.length) {
+                const operator = elements[i];
+                // Skips if it's not the current operators we're processing.
+                if (operators[operator]) {
+                    // Factorization
+                    elements[i - 1] = operators[operator](elements[i - 1], elements[i + 1]);
+                    elements.splice(i, 2);
+                }
+                else
+                    i += 2;
+            }
+        });
+
+        return elements[0];
     }
 }
